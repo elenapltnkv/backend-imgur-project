e895c8fd26075ec3be6309b7a036e46ea088cd3a
elenapltnkv <elenapltnkv@gmail.com>
2024-06-14 16:26:10 +0300

    gb backend imgur project refactor URL test

--- src/test/java/ru/elenapltnkv/imgurTest/ImageUrlTest.java
+++ src/test/java/ru/elenapltnkv/imgurTest/ImageUrlTest.java
@@ -1,10 +1,13 @@
 package ru.elenapltnkv.imgurTest;
 
 import io.restassured.mapper.ObjectMapperType;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 
 import static io.restassured.RestAssured.given;
@@ -13,13 +16,15 @@ public class ImageUrlTest extends BaseTest {
     String imageDeleteHash;
 
     @Test
-    void uploadImageUrlTest() {
-        String URL = "https://pictures.pibig.info/uploads/posts/2023-04/1680603592_pictures-pibig-info-p-realistichnie-risunki-zhivotnikh-instagram-35.jpg";
+    void uploadImageUrlTest() throws FileNotFoundException {
+        String imgURL = "https://pictures.pibig.info/uploads/posts/2023-04/1680603592_pictures-pibig-info-p-realistichnie-risunki-zhivotnikh-instagram-35.jpg";
         imageDeleteHash = given()
                 .header("Authorization", token)
-                .body(URL)
+                .formParam("image",imgURL)
+                //.body()
                 .expect()
                 .statusCode(200)
+                .body("data.type", CoreMatchers.equalTo("image/jpeg"))
                 .when()
                 .post("/upload")
                 .prettyPeek()
